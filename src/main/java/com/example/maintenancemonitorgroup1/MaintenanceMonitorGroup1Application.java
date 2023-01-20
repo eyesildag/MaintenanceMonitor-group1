ad7f2c2bf3ff1df4b7c091fb148eb3f2a03380cb
Emre Yesildag <e12120490@student.tuwien.ac.at>
2023-01-20 17:07:29 +0100

    created empty project

---
+++ src/main/java/com/example/maintenancemonitorgroup1/MaintenanceMonitorGroup1Application.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorgroup1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorGroup1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorGroup1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenancemonitorgroup1/MaintenanceMonitorGroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorgroup1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorGroup1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
